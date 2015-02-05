7724bcd9ae4c8045b4ff256ca9a8d33de55b167b
jjzhai <jjzhai@lisystec.com>
2015-02-05 16:34:32 +0800

    first commit

---
+++ src/com/jjzhai/common/StartSelenium.java
@@ -0,0 +1,79 @@
+package com.jjzhai.common;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+
+public class StartSelenium {
+	public DefaultSelenium start() {
+		String filePathString="src/SeleniumConnectArguments.properties";
+		
+		String serverHost="";
+		int serverPort;
+		String browserString="";
+		String browserURL="";
+		
+		Properties properties = new Properties();
+		
+		try {
+			InputStream inputStream = new FileInputStream(filePathString);					
+			properties.load(inputStream);								
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		serverHost = properties.getProperty("serverHost", "localhost");
+		serverPort = Integer.parseInt(properties.getProperty("serverPort","4445"));
+		browserString = properties.getProperty("browserString","*iexplore");
+		browserURL = properties.getProperty("browserURL","http://www.baidu.com");
+		
+		DefaultSelenium selenium=new DefaultSelenium(serverHost, serverPort, browserString, browserURL);
+		selenium.start();
+		return selenium;
+	}
+	
+	public DefaultSelenium startAssignBrowser(String browserType) {
+		String filePathString="src/SeleniumConnectArguments.properties";
+		
+		String serverHost="";
+		int serverPort;
+		String browserString="";
+		String browserURL="";
+		
+		Properties properties = new Properties();
+		
+		try {
+			InputStream inputStream = new FileInputStream(filePathString);					
+			properties.load(inputStream);								
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		serverHost = properties.getProperty("serverHost", "localhost");
+		serverPort = Integer.parseInt(properties.getProperty("serverPort","4445"));
+		if(browserType.equalsIgnoreCase("IE")){
+			browserString = properties.getProperty("browserIEString");			
+		}
+		else if(browserType.equalsIgnoreCase("Firefox")){
+			browserString = properties.getProperty("browserFirefoxString");
+		}		
+		else if(browserType.equalsIgnoreCase("Safari")){
+			browserString = properties.getProperty("browserSafariString");
+		}
+		else if(browserType.equalsIgnoreCase("Opera")){
+			browserString = properties.getProperty("browserOperaString");
+		}
+		else{
+			browserString = properties.getProperty("browserChromeString");
+		}		
+		browserURL = properties.getProperty("browserURL","http://www.baidu.com");
+		
+		DefaultSelenium selenium=new DefaultSelenium(serverHost, serverPort, browserString, browserURL);
+		selenium.start();
+		return selenium;
+	}
+}
