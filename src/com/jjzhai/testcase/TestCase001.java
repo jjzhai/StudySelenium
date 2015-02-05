cfb8b4ed1ed6abb572712f18c40749c12dbcf465
jjzhai <jjzhai@lisystec.com>
2015-02-05 18:08:27 +0800

    test

---
+++ src/com/jjzhai/testcase/TestCase001.java
@@ -0,0 +1,56 @@
+package com.jjzhai.testcase;
+
+import com.jjzhai.common.StartSelenium;
+import com.thoughtworks.selenium.DefaultSelenium;
+
+public class TestCase001 {
+
+	/**
+	 * @param args
+	 */
+	public void testCase() {
+		// TODO Auto-generated method stub	
+		DefaultSelenium selenium = null;
+		StartSelenium startSelenium = new StartSelenium();
+		//startSelenium.start(selenium);	
+		selenium = startSelenium.startAssignBrowser("firefox");
+		//selenium.windowFocus();
+		selenium.open("http://www.baidu.com");
+		selenium.open("http://www.hao123.com");
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		selenium.goBack();
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		selenium.type("id=kw", "∫Œ“‘ÛœÛÔƒ¨");
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		selenium.refresh();
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		selenium.windowMaximize();
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		selenium.close();
+	}
+}
