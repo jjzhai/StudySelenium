0297d0a8e357b2a8b79d379c4ad46d197882f68e
jjzhai <jjzhai@lisystec.com>
2015-02-09 16:57:09 +0800

    start selenium server(run bat)

--- files/startSeleniumServer.bat
+++ files/startSeleniumServer.bat
@@ -1,3 +1,3 @@
 d:
 cd D:\software\selenium-server
-java -jar D:\software\selenium-server\selenium-server-standalone-2.44.0.jar -port 4445
+java -jar selenium-server-standalone-2.44.0.jar -port 4445
